0f3d4963c51abee87c579dea6bdd8a28e72f282c
Alfredo Monclus <alfredomonclus@gmail.com>
2017-10-16 06:59:46 -0300

    Initial commit

---
+++ RA_ROM_cleaner_tester.ps1
@@ -0,0 +1,85 @@
+# Alfredo Monclús
+cls
+#
+##
+### Carpeta con roms
+
+$rom_path="D:\Documents\Descargas\No-intro"
+
+###
+##
+#
+$playlist_path="$env:appdata\RetroArch\playlists"
+
+function limpiar
+{
+	#limpiar subcarpeta_de_roms playlist
+	param([string]$roms_path, [string]$playlist_name)
+	
+	Write-Host "Limpiando",$playlist_name -ForegroundColor yellow
+	$roms_path=$rom_path+$roms_path
+	$playlist=$playlist_path+"\"+$playlist_name+".lpl"
+	$roms = Get-ChildItem $roms_path\* -Include *.7z, *.zip
+	for ($i=0; $i -lt $roms.Count; $i++)
+    {
+		#rom clean
+		$romname=$roms[$i].name
+        if ("$romname" -like "neogeo.*") {continue}
+		if ("$romname" -like "qsound.*") {continue}
+		if ("$romname" -clike "*BIOS*") {continue}
+		
+		$playlist_item=$(Select-String -path "$playlist" -quiet -simplematch "$romname")
+		if (-not $playlist_item )
+		{
+			Write-Host $roms[$i].name,"será eliminado" -ForegroundColor Red
+			
+			
+		}
+	}
+	for ($i=0; $i -lt 3; $i++) 
+	{
+
+		#thumbnail clean
+		if ($i -eq 0) {$thumb_path="$env:appdata\RetroArch\thumbnails\"+$playlist_name+"\Named_Boxarts"}
+		if ($i -eq 1) {$thumb_path="$env:appdata\RetroArch\thumbnails\"+$playlist_name+"\Named_Snaps"}
+		if ($i -eq 2) {$thumb_path="$env:appdata\RetroArch\thumbnails\"+$playlist_name+"\Named_Titles"}
+		$thumb = Get-ChildItem $thumb_path\* -Include *.png
+		for ($j=0; $j -lt $thumb.Count; $j++)
+		{
+			$thumbname=$thumb[$j].name.split(".")[0]
+			$thumbname=$thumbname.split("_")[0]
+			$playlist_item=$(Select-String -literalpath "$playlist" -quiet -simplematch "$thumbname")
+			if (-not $playlist_item )
+			{
+				Write-Host $playlist_name,$thumb[$j].name,"Miniatura eliminada" -ForegroundColor Magenta
+			}
+		}
+	}
+}
+
+#limpiar "subcarpeta_de_roms" "nombre de la playlist"
+
+limpiar "\MAME\Arcade - Action" "Arcade - Action"
+limpiar "\MAME\Arcade - BeatEmUp" "Arcade - BeatEmUp"
+limpiar "\MAME\Arcade - Driving" "Arcade - Driving"
+limpiar "\MAME\Arcade - Fighting" "Arcade - Fighting"
+limpiar "\MAME\Arcade - Mature" "Arcade - Mature"
+limpiar "\MAME\Arcade - Maze" "Arcade - Maze"
+limpiar "\MAME\Arcade - Misc" "Arcade - Misc"
+limpiar "\MAME\Arcade - Platform" "Arcade - Platform"
+limpiar "\MAME\Arcade - PlatformSingle" "Arcade - PlatformSingle"
+limpiar "\MAME\Arcade - Puzzle" "Arcade - Puzzle"
+limpiar "\MAME\Arcade - Sports" "Arcade - Sports"
+limpiar "\MAME\Arcade - STGHori" "Arcade - STGHori"
+limpiar "\MAME\Arcade - STGMisc" "Arcade - STGMisc"
+limpiar "\MAME\Arcade - STGSingle" "Arcade - STGSingle"
+limpiar "\MAME\Arcade - STGVert" "Arcade - STGVert"
+limpiar "\MAME\Arcade - Lightgun" "Arcade - Lightgun"
+limpiar "\N64" "Nintendo - Nintendo 64"
+limpiar "\GBA" "Nintendo - Game Boy Advance"
+limpiar "\GENESIS" "Sega - Mega Drive - Genesis"
+limpiar "\fgc-NES" "Nintendo - Nintendo Entertainment System"
+limpiar "\fgc-SNES" "Nintendo - Super Nintendo Entertainment System"
+
+echo "Finished"
+pause
---
+++ RA_cores_updater.ps1
@@ -0,0 +1,72 @@
+# Retroarch Core updater for Windows
+#
+# Based on linux script of github.com/cpinkus
+# 
+# Alfredo Monclús (2017)
+
+cls
+
+# Location variables
+$cores_url="http://buildbot.libretro.com/nightly/windows/x86_64/latest"
+$cores_path="$env:appdata\retroarch\cores"
+$logfile="$env:appdata\retroarch\cores\cores_updater.log"
+
+# Prepare files
+if(-not(Test-Path "$cores_path\.timestamps.old")){$null > "$cores_path\.timestamps.old"}
+if(-not(Test-Path "$cores_path\.timestamps.new")){$null > "$cores_path\.timestamps.new"}
+mv "$cores_path\.timestamps.new" "$cores_path\.timestamps.old" -Force
+# Grab timestamps from URL
+Start-BitsTransfer -Source $cores_url/.index-extended -Destination $cores_path\.timestamps.new -TransferType Download
+
+# Check if downloaded
+if ($(Test-Path "$cores_path\.timestamps.new"))
+{
+
+	# New logfile entry
+	echo "-- $(date) --" >> "$logfile"
+
+	# Download and replace cores
+	$cores = Get-ChildItem $cores_path -File *.dll
+	for ($i=0; $i -lt $cores.Count; $i++) {
+		if (-not($cores[$i].name.Contains("_libretro"))) {continue}
+		$corename=$cores[$i].name
+		$timestamp_old=$(Get-Content .\.timestamps.old | Select-String -simplematch $corename)
+		$timestamp_new=$(Get-Content .\.timestamps.new | Select-String -simplematch $corename)
+		$current_timestamp=$timestamp_new.ToString($timestamp_new).Split(" ")[0]
+		if ( "$timestamp_new" -eq "$timestamp_old" )
+		{
+			Write-Host $current_timestamp,"SKIPPED ",$corename -ForegroundColor Gray
+			Write-output $current_timestamp`t"SKIPPED "`t$corename >> $logfile
+			continue
+		}
+
+		$core_full_url=$cores_url + "/" + $corename + ".zip"
+		Start-BitsTransfer -Source $core_full_url -Destination $cores_path\$corename.zip -TransferType Download
+		
+		# Check if downloaded
+		if ($(Test-Path $cores_path\$corename.zip))
+		{
+			rm $corename
+			Expand-Archive "$cores_path\$corename.zip" "$cores_path" -force
+			rm "$cores_path\$corename.zip"
+				
+			
+			Write-Host $current_timestamp,"UPDATED ",$corename  -ForegroundColor Green
+			Write-output $current_timestamp`t"UPDATED "`t$corename >> $logfile
+		}
+		else
+		{
+			Write-Host $current_timestamp,"FAILED ",$corename  -ForegroundColor Red
+			Write-output $current_timestamp`t"FAILED "`t$corename >> $logfile
+		}
+
+	}
+	echo "Done"
+}
+else
+{
+	Write-Host "FAILED to download timestamps" -ForegroundColor Red
+}
+
+sleep 4
+pause
